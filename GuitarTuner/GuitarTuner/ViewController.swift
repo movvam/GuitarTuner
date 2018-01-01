21f25309e7f992d5f12d79af55cf078e9f25cc49
Manikanth Movva <manikanthmovva@gmail.com>
2017-12-31 22:19:47 -0800

    added general framework for audio playing

--- GuitarTuner/GuitarTuner/ViewController.swift
+++ GuitarTuner/GuitarTuner/ViewController.swift
@@ -11,10 +11,20 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    var player: AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do{
+            let audioPath = Bundle.main.path(forResource: "A2", ofType: ".mp3")
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+        }catch{
+            print("error")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
