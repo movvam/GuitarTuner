e700e164fe1cff35c092e784b0aa3a0e7f78441d
Manikanth Movva <manikanthmovva@gmail.com>
2018-01-07 17:24:38 -0800

    styling fixes

--- GuitarTuner/GuitarTuner/ViewController.swift
+++ GuitarTuner/GuitarTuner/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var dropSwitch: UISwitch!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
